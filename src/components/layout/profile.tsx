c9e66fad326753ff623d592e283377806845cb68
jihwan-kim3 <jihwan_kim3@kolon.com>
2024-11-21 16:30:11 +0900

    passkeys 등록 버튼 및 기능

--- src/components/layout/profile.tsx
+++ src/components/layout/profile.tsx
@@ -1,13 +1,76 @@
 import { useAuth } from "@/lib/auth/auth-provider";
 import { Dropdown, MenuProps } from "antd";
-import { ChevronDown, LogOut, User } from "lucide-react";
+import { ChevronDown, KeyRound, LogOut, User } from "lucide-react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import React, { useCallback } from "react";
 
 const Profile = () => {
   const { session } = useAuth();
+  console.log("session", session);
+  const base64ToArrayBuffer = (base64: string) => {
+    const binaryString = window.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+  };
+  const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer) as any));
+  };
+  const handleRegister = async () => {
+    try {
+      // Step 1: Request registration options from the server
+      console.log("step2");
+      const registerOptionsResponse = await fetch("/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ action: "registerOptions", userId: session.user.login }),
+      });
+      const registerOptions = await registerOptionsResponse.json();
 
+      // Convert the server response from Base64 into ArrayBuffers
+      registerOptions.challenge = base64ToArrayBuffer(registerOptions.challenge);
+      registerOptions.user.id = base64ToArrayBuffer(window.btoa(registerOptions.user.id));
+      // Step 2: Create a new credential
+      const newCredential: PublicKeyCredential = (await navigator.credentials.create({
+        publicKey: registerOptions,
+      })) as PublicKeyCredential;
+
+      // Prepare the data to be pushed to backend (converting ArrayBuffers back to Base64)
+      const credential = {
+        id: newCredential.id,
+        rawId: btoa(String.fromCharCode(...new Uint8Array(newCredential.rawId))),
+        response: {
+          attestationObject: arrayBufferToBase64((newCredential.response as any).attestationObject),
+          clientDataJSON: arrayBufferToBase64(newCredential.response.clientDataJSON),
+        },
+        type: newCredential.type,
+      };
+      console.log("step3");
+      // Step 3: Register the credential with the server
+      const registerResponse = await fetch("/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: "registerCredential",
+          userId: session.user.login,
+          credential,
+        }),
+      });
+      console.log("registerResponse", registerResponse);
+      console.log(1);
+      if (registerResponse.ok) {
+        console.log(2);
+      } else {
+        console.log(3);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const handleLogoutClick = useCallback(async () => {
     signOut({ callbackUrl: "/login" });
   }, []);
@@ -23,12 +86,26 @@ const Profile = () => {
     },
     {
       label: (
+        <div
+          className="link-with-icon"
+          onClick={() => {
+            handleRegister();
+          }}
+        >
+          <KeyRound width={16} height={16} />
+          <div>패스키 등록</div>
+        </div>
+      ),
+      key: "1",
+    },
+    {
+      label: (
         <a onClick={handleLogoutClick} className="link-with-icon">
           <LogOut width={16} height={16} />
           로그아웃
         </a>
       ),
-      key: "1",
+      key: "2",
     },
   ];
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,78 +2,8 @@ import GradientBg from "@/components/page/login/gradient-bg";
 import LoginForm from "@/components/page/login/login-form";
 import { Alert } from "antd";
 import { Verified } from "lucide-react";
-import { useState } from "react";
 
 const LoginPage = () => {
-  const [userId, setUserId] = useState("");
-  const [message, setMessage] = useState("");
-  const base64ToArrayBuffer = (base64: string) => {
-    const binaryString = window.atob(base64);
-    const len = binaryString.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-  };
-  const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
-    return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer) as any));
-  };
-  const handleRegister = async () => {
-    try {
-      // Step 1: Request registration options from the server
-      console.log("step2");
-      const registerOptionsResponse = await fetch("/api/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ action: "registerOptions", userId }),
-      });
-      const registerOptions = await registerOptionsResponse.json();
-
-      // Convert the server response from Base64 into ArrayBuffers
-      registerOptions.challenge = base64ToArrayBuffer(registerOptions.challenge);
-      registerOptions.user.id = base64ToArrayBuffer(window.btoa(registerOptions.user.id));
-      // Step 2: Create a new credential
-      const newCredential: PublicKeyCredential = (await navigator.credentials.create({
-        publicKey: registerOptions,
-      })) as PublicKeyCredential;
-
-      // Prepare the data to be pushed to backend (converting ArrayBuffers back to Base64)
-      const credential = {
-        id: newCredential.id,
-        rawId: btoa(String.fromCharCode(...new Uint8Array(newCredential.rawId))),
-        response: {
-          attestationObject: arrayBufferToBase64((newCredential.response as any).attestationObject),
-          clientDataJSON: arrayBufferToBase64(newCredential.response.clientDataJSON),
-        },
-        type: newCredential.type,
-      };
-      console.log("step3");
-      // Step 3: Register the credential with the server
-      const registerResponse = await fetch("/api/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          action: "registerCredential",
-          userId,
-          credential,
-        }),
-      });
-      console.log("registerResponse", registerResponse);
-      console.log(1);
-      if (registerResponse.ok) {
-        console.log(2);
-        setMessage("Registration successful! You can now login");
-      } else {
-        console.log(3);
-        setMessage("Registration failed.");
-      }
-    } catch (error) {
-      console.error(error);
-      setMessage("An error occurred during registration222");
-    }
-  };
-
   return (
     <div className="flex min-h-screen bg-white items-centerw-full">
       <div className={`relative hidden w-1/2 lg:block`}>
@@ -112,7 +42,6 @@ const LoginPage = () => {
               <h2 className="mt-2 text-5xl font-bold leading-tight inter">AWESOME</h2>
               <div className="mt-1 text-lg text-gray-400">Admin System</div>
             </div>
-            <button onClick={handleRegister}>passkeys 등록</button>
             <div className="w-full px-2 mt-12 sm:px-6">
               <LoginForm />
             </div>
